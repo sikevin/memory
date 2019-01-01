d1a73751d22b07a0500c035590e2c436ef382998
Kevin SI <sikevin.sk@gmail.com>
2019-01-01 18:52:01 +0100

    init React with react create app & add some cards

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import GuessCount from './GuessCount'
+import Card from './Card'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	handleCardClick(card){
+		console.log(card, 'clicked')
+	}
+
+	render() {
+		return (
+			<div className="memory">
+				<GuessCount guesses={0} />
+				<Card card="🚀" feedback="hidden" onClick={this.handleCardClick}/>
+				<Card card="🌞" feedback="justMatched" onClick={this.handleCardClick}/>
+				<Card card="🌎" feedback="justMismatched" onClick={this.handleCardClick}/>
+				<Card card="🌜" feedback="visible" onClick={this.handleCardClick}/>
+				<Card card="✨" feedback="hidden" onClick={this.handleCardClick}/>
+				<Card card="☄️" feedback="justMatched" onClick={this.handleCardClick}/>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
